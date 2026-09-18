f9350630c249649406534053f32b8f1894004ed6
GabrielCleto <gabrielhenrique.cl2002@gmail.com>
2026-09-17 22:16:10 -0300

    CORRIGINDO Exercicios

--- aula03/exercicio2.ts
+++
@@ -1,9 +0,0 @@
-
-
-let cidades: string[] = ['São Paulo', 'Rio de Janeiro', 'Belo Horizonte', 'Curitiba', 'Salvador'];
-
-function listarCidades(nomes: string[]): void {
-  nomes.forEach(cidade => console.log(cidade));
-}
-
-listarCidades(cidades);
